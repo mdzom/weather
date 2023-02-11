9ac3f67c92f55deae9756c69262a6658a21de871
Gennadiy <ggeeena@gmail.com>
2023-02-11 18:33:39 +1000

    first commit

---
+++ weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weather
+//
+//  Created by Gennadij Pleshanov on 11.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
